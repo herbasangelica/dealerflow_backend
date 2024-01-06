a2b7e190712b93f08af0f13cbfb78054d624496c
herbasangelica <angelicahcueva@gmail.com>
2024-01-06 18:05:23 +0800

    Modify the customer model

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Customer extends Model
 {
     use HasFactory;
+
+
+    protected $fillable = [
+        'customerName',
+        'customerAddr',
+        'customerPhone',
+        'customerGender',
+        'customerAnnualIncome',
+    ];
+
+    public function Sale()
+    {
+        return $this->hasMany(Sale::class);
+    }
 }
